cc5d8c0003e3689c06f2444febf02048154236b9
Prashant-treat <prashantreat@gmail.com>
2026-02-14 01:20:50 +0530

    email config

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER, // your email
         pass: process.env.EMAIL_PASS, // app password
